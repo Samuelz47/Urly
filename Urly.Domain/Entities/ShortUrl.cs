22f57010e638f3d2c84f9ee4b3d274ce76fa7fe0
Samuel Gomes <samuelgms.lt@gmail.com>
2025-10-28 22:04:15 -0300

    feat: Define initial domain entities (ShortUrl, UrlClick)

---
+++ Urly.Domain/Entities/ShortUrl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urly.Domain.Entities;
+public class ShortUrl
+{
+    public int Id { get; set; }
+    [Required]
+    public string LongURL { get; set; }
+    [Required]
+    public string ShortCode { get; set; }
+    public DateTime CreateAtUtc { get; set; }
+    public int? UserId { get; set; }
+}
---
+++ Urly.Domain/Entities/UrlClick.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urly.Domain.Entities;
+public class UrlClick
+{
+    public int Id { get; set; }
+    public int ShortUrlId { get; set; }
+    public DateTime ClickedAtUtc { get; set; }
+}
