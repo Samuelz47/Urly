98d302921fa35af68820279f037ce1e04385846c
Samuel Gomes <samuelgms.lt@gmail.com>
2025-11-01 18:20:16 -0300

    feat: Define repository interfaces for Domain

---
+++ Urly.Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urly.Domain.Repositories;
+public interface IRepository<T>
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+    T Create(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+}
---
+++ Urly.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urly.Domain.Repositories;
+public interface IUnitOfWork
+{
+    Task<int> CommitAsync(CancellationToken cancellationToken = default);
+}
